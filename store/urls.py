5d28ebedcda3bdb973308cde6569843a19fd03d1
zhacyn <tsynduev@mail.ru>
2024-01-31 21:29:27 +0300

    Lab 5_1.2

--- store/urls.py
+++ store/urls.py
@@ -2,17 +2,21 @@
 from django.urls import path
 from .views import products_view
 from .views import products_page_view
+from .views import shop_view
 from .views import cart_view
 from .views import cart_add_view
-from .views import cart_del_view
+from .views import cart_del_view, delivery_estimate_view, coupon_check_view
 
 app_name = 'store'
 
 urlpatterns = [
     path('product/', products_view),
+    path('', shop_view, name="shop_view"),
     path('product/<slug:page>.html', products_page_view, name="products_page_view"),
     path('product/<int:page>', products_page_view),
-    path('cart/', cart_view),
+    path('cart/', cart_view, name="cart_view"),
     path('cart/add/<str:id_product>', cart_add_view),
     path('cart/del/<str:id_product>', cart_del_view),
+    path('delivery/estimate/', delivery_estimate_view),
+    path('coupon/check/<slug:name_coupon>', coupon_check_view),
 ]
